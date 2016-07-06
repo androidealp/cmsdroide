7eba59233e1c7395c6bee992f9c891aeb77456bd
androidealp <and4563@gmail.com>
2016-07-06 00:35:27 -0300

    ajustes galeria

---
+++ components/widgets/droideffects/Effects.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace app\components\widgets\droideffects;
+
+use yii\base\Widget;
+use yii\helpers\Html;
+
+class Effects extends Widget {
+	public $options = [];
+
+
+	public function init(){
+
+		
+	}
+
+	public function run(){
+
+		echo "meu efeito";
+	}
+
+
+
+
+}
