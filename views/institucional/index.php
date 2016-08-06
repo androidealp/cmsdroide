402425193bec431b70f119934f489ea067a10eb3
phoenix <and4563@gmail.com>
2016-08-06 14:39:20 -0300

    criei o sistema de clones usando o vue

--- views/institucional/index.php
+++ views/institucional/index.php
@@ -1,3 +1,17 @@
 
 
 <h1>teste</h1>
+
+
+<div id="clones">
+
+    <h3>clone input</h3>
+    Adicionar item  <button @click="addItem">+</button>
+
+     <ul>
+       <li v-for="clone in clones">
+         <input type="text" name="{{clone.name}}" value="">
+
+         <button v-on:click="relItem($index)">X</button>
+       </li>
+</div>
--- web/temas/bootstrapdroide/js/template.js
+++ web/temas/bootstrapdroide/js/template.js
@@ -1,3 +1,28 @@
 $(document).on('click','[data-clone]',function(e){
   e.preventDefault();
 });
+
+// normal = new Vue({
+//   el: '#app',
+//   data: {
+//     mensagem: 'nova mensagem inserida via VUE',
+//   }
+// });
+
+ new Vue({
+  el: '#clones',
+  data:{
+    clones:[]
+  },
+  methods: {
+    addItem: function () {
+
+      this.clones.push({ 'name':'inputText[]'});
+
+    },
+    relItem: function (index) {
+      this.clones.splice(index, 1)
+    }
+  }
+
+});
